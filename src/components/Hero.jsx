c29914cd6503e9406e8a6a7f49f55163acd8f877
VictoriaBel788 <valerovictoria299@gmail.com>
2026-04-24 15:35:29 -0500

    feat: guardar reservas en local-storage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "../styles/Hero.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <section className="hero">
       <h2 className="subtitle">El arte de la<br /> paciencia</h2>
       <p className="description">
-        Descubre la serenidad a través del cuidado y diseño de árboles Bonsái. 
+        Descubre la serenidad a través del cuidado y diseño de árboles Bonsái.
         <br />Un espacio para reconectar con la naturaleza.
       </p>
-      <button className="btn">Reserva un taller</button>
+      <Link to="/reservations" className="boton">
+        Reserva un taller
+      </Link>
     </section>
   );
 };
--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,10 +1,27 @@
 
-import React from 'react'
+import { useState } from 'react'
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import '../styles/Reservations.css'
+import { saveLocalStorage } from "../helpers/local-storage"
 
 function Reservations() {
+
+  const [nombre, setNombre] = useState("")
+  const [correo, setCorreo] = useState("")
+  const [nivel, setNivel] = useState("")
+  const [mensaje, setMensaje] = useState("")
+
+  function guardarReserva() {
+  const data = {
+    nombre,
+    correo,
+    nivel,
+    mensaje
+  }
+
+  saveLocalStorage("reserva", data)
+}
   return (
     <>
       <NavBar />
@@ -23,17 +40,17 @@ function Reservations() {
 
               <div className="form-group">
                 <label>Nombre completo</label>
-                <input type="text" placeholder="Ej. Ana Silva" />
+                <input type="text" placeholder="Ej. Ana Silva" onChange={(e) => setNombre(e.target.value)} />
               </div>
 
               <div className="form-group">
                 <label>Correo electrónico</label>
-                <input type="email" placeholder="tu@email.com" />
+                <input type="email" placeholder="tu@email.com" onChange={(e) => setCorreo(e.target.value)} />
               </div>
 
               <div className="form-group">
                 <label>Nivel de experiencia</label>
-                <select>
+                <select onChange={(e) => setNivel(e.target.value)}>
                   <option>Principiante (Nunca he tenido un Bonsái)</option>
                   <option>Intermedio (Tengo algunos árboles)</option>
                   <option>Avanzado (Busco perfeccionar técnicas)</option>
@@ -42,10 +59,12 @@ function Reservations() {
 
               <div className="form-group">
                 <label>Mensaje (Opcional)</label>
-                <textarea placeholder="¿Qué te gustaría aprender?"></textarea>
+                <textarea placeholder="¿Qué te gustaría aprender?" onChange={(e) => setMensaje(e.target.value)}></textarea>
               </div>
 
-              <button type="submit">Enviar Solicitud</button>
+              <button type="submit" onClick={guardarReserva}>
+                Enviar Solicitud
+              </button>
 
             </form>
           </div>
